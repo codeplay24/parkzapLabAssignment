f45dae1a8bb267413ee8f8c4fe61a36a4bc5f519
Rajdeep Sarkar <rajdeepsarkar1d@gmail.com>
2021-01-04 10:22:21 +0530

    Database env changed

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const url = 'mongodb://127.0.0.1:27017/ParkzapDB'
-mongoose.connect(process.env.MONGODB_URI || url, {
+
+mongodb+srv://rahul-uses-DB:8wdLEUSZIvoJ0mMl@firstcluster.emyrj.mongodb.net/ParkzapDB?retryWrites=true&w=majority
+
+mongoose.connect(process.env.MONGODB_URL || url, {
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useCreateIndex:true
