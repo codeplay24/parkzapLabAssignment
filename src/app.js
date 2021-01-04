811a5068ffe52325e291b71c17cc06fe071e00a3
Rajdeep Sarkar <rajdeepsarkar1d@gmail.com>
2021-01-04 12:19:59 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -24,49 +24,62 @@ app.set('views', viewsPath)
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+app.get('', (req,res)=>{
+  res.redirect('/user-form')
+})
+
 app.get('/user-form', (req,res)=>{
   res.render('index')
 })
 
 app.post('/dashboard', async(req,res)=>{
-  const number = req.body.phoneNumber
-  var phoneno = /^\d{10}$/;
-  if(!number.match(phoneno)){
-    res.send('<a href="/user-form">invalid phone number</a>')
-    return
-  }
-  const user = new userModel({
-    name:req.body.name,
-    email:req.body.email,
-    phoneNumber:req.body.phoneNumber,
-    dob:req.body.birthday
-  })
-  await user.save()
-  const transporter = nodemailer.createTransport({
-      service:'gmail',
-      auth:{
-        user:'rajdeepsarkar1d@gmail.com',
-        pass: process.env.MAIL_PASSWORD
-      }
-    })
-    var mailOptions = {
-      from:'rajdeepsarkar1d@gmail.com',
-      to:req.body.email,
-      subject:'Verification',
-      text:'Welcome to parkzap labs'
+  try{
+    const number = req.body.phoneNumber
+    var phoneno = /^\d{10}$/;
+    if(!number.match(phoneno)){
+      res.send('<a href="/user-form">invalid phone number</a>')
+      return
     }
-    transporter.sendMail(mailOptions, (err,succ)=>{
-      if(err){
-        throw new Error('something bad happened while sending the mail')
-      }
-      console.log('email sent successfully')
+    const user = new userModel({
+      name:req.body.name,
+      email:req.body.email,
+      phoneNumber:req.body.phoneNumber,
+      dob:req.body.birthday
     })
-  res.redirect('/getDashboard')
+    await user.save()
+    console.log('comes here');
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+          user:'rajdeepsarkar1d@gmail.com',
+          pass: process.env.MAIL_PASSWORD
+        }
+      })
+      var mailOptions = {
+        from:'rajdeepsarkar1d@gmail.com',
+        to:req.body.email,
+        subject:'Verification',
+        text:'Welcome to parkzap labs'
+      }
+      transporter.sendMail(mailOptions, (err,succ)=>{
+        if(err){
+          throw new Error('something bad happened while sending the mail')
+        }
+        console.log('email sent successfully')
+      })
+    res.redirect('/getDashboard')
+  }catch(e){
+    res.status(404).send(e)
+  }
 })
 
 app.get('/getDashboard', async(req,res)=>{
-  const users = await userModel.find({}).lean()
-  res.render('dashboard', {userArr:users})
+  try{
+    const users = await userModel.find({}).lean()
+    res.render('dashboard', {userArr:users})
+  }catch(e){
+    res.status(404).send(e)
+  }
 })
 
 app.listen(port, ()=>{
